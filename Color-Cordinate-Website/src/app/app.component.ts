271d724883ccf866485f5c765462bd09c0a20c8b
Brian Wellette <c837317580@frankfort.cs.colostate.edu>
2025-04-06 19:02:47 -0600

    updated files

--- Color-Cordinate-Website/src/app/app.component.ts
+++ Color-Cordinate-Website/src/app/app.component.ts
@@ -1,11 +1,10 @@
 // Running router that checks for links
 
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { RouterModule } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { AboutComponent } from './about/about.component';
- 
+
+
 @Component({
   selector: 'app-root',
   imports: [NavbarComponent, RouterModule, RouterOutlet],
@@ -13,5 +12,6 @@ import { AboutComponent } from './about/about.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'IC9\'s Homepage'; 
+  title = 'ICE9\'s Homepage';
+
 }
